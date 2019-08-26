67ec291169bd55180ec9aea28efe288d90da315c
C Hdz <47310270+charlieitesm@users.noreply.github.com>
2019-08-25 19:37:39 -0500

    Add first version of Q2

--- assignment1-1/q2.go
+++ assignment1-1/q2.go
@@ -3,6 +3,8 @@ package cos418_hw1_1
 import (
 	"bufio"
 	"io"
+	"log"
+	"os"
 	"strconv"
 )
 
@@ -10,8 +12,12 @@ import (
 // You should only output to `out` once.
 // Do NOT modify function signature.
 func sumWorker(nums chan int, out chan int) {
-	// TODO: implement me
 	// HINT: use for loop over `nums`
+	result := 0
+	for i := range nums {
+		result += i
+	}
+	out <- result
 }
 
 // Read integers from the file `fileName` and return sum of all values.
@@ -20,10 +26,50 @@ func sumWorker(nums chan int, out chan int) {
 // You should use `checkError` to handle potential errors.
 // Do NOT modify function signature.
 func sum(num int, fileName string) int {
-	// TODO: implement me
 	// HINT: use `readInts` and `sumWorkers`
 	// HINT: used buffered channels for splitting numbers between workers
-	return 0
+	file, err := os.Open(fileName)
+	defer func() {
+		err = file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	numbers, err := readInts(file)
+
+	// We'll set the buffer of the input channel to 1 in order to better split work across the available workers
+	inputWorkerChan := make(chan int, 1)
+	outputWorkerChan := make(chan int)
+
+	// Instantiate the specified number of workers
+	for i := 0; i < num; i++ {
+		go sumWorker(inputWorkerChan, outputWorkerChan)
+	}
+
+	// Select will block until we have a slot available in the input channel, once there is, we pass on work to the map
+	//  workers
+	for _, n := range numbers {
+		select {
+		case inputWorkerChan <- n:
+			continue
+		}
+	}
+	// Let the workers know there are no more numbers coming
+	close(inputWorkerChan)
+
+	// Process the partial results into the final result (reduce operation)
+	result := 0
+	for i := 0; i < num; i++ {
+		preliminaryResult := <-outputWorkerChan
+		log.Printf("Processing partial result %d from worker...", preliminaryResult)
+		result += preliminaryResult
+	}
+
+	return result
 }
 
 // Read a list of integers separated by whitespace from `r`.
